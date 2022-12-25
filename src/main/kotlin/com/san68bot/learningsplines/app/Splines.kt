9a6b66c7721466e94404335060dec185b3ef9db4
San68bot <san68bot@gmail.com>
2022-12-25 11:25:08 -0500

    COLORS

--- src/main/kotlin/com/san68bot/learningsplines/app/Splines.kt
+++ src/main/kotlin/com/san68bot/learningsplines/app/Splines.kt
@@ -23,10 +23,10 @@ class Splines : Application() {
 
     private fun splines() {
         Bezier(mainPane, arrayListOf(
-            DynamicPoint(300.0, 500.0, 8.0, "Point 1", BetterColors.blue, BetterColors.orange),
-            DynamicPoint(350.0, 300.0, 8.0, "Point 2", BetterColors.blue, BetterColors.orange),
-            DynamicPoint(500.0, 300.0, 8.0, "Point 3", BetterColors.blue, BetterColors.orange),
-            DynamicPoint(550.0, 500.0, 8.0, "Point 4", BetterColors.blue, BetterColors.orange)
+            DynamicPoint(300.0, 500.0, 8.0, "Point 1", BetterColors.light_blue, BetterColors.orange),
+            DynamicPoint(350.0, 300.0, 8.0, "Point 2", BetterColors.light_blue, BetterColors.orange),
+            DynamicPoint(500.0, 300.0, 8.0, "Point 3", BetterColors.light_blue, BetterColors.orange),
+            DynamicPoint(550.0, 500.0, 8.0, "Point 4", BetterColors.light_blue, BetterColors.orange)
         ), Bezier.CalculationMethod.bernstein)
     }
 
--- src/main/kotlin/com/san68bot/learningsplines/graphics/BetterColors.kt
+++ src/main/kotlin/com/san68bot/learningsplines/graphics/BetterColors.kt
@@ -3,9 +3,24 @@ package com.san68bot.learningsplines.graphics
 import javafx.scene.paint.Color
 
 object BetterColors {
-    val green: Color = Color.rgb(75, 228, 135)
-    val orange: Color = Color.rgb(252, 188, 68)
-    val red: Color = Color.rgb(252, 13, 27)
-    val blue: Color = Color.rgb(19, 122, 227)
-    val purple: Color = Color.rgb(170, 39, 251)
+    val green: Color =
+        Color.rgb(75, 228, 135)
+
+    val orange: Color =
+        Color.rgb(252, 188, 68)
+
+    val yellow: Color =
+        Color.rgb(249, 225, 102)
+
+    val red: Color =
+        Color.rgb(229, 15, 87)
+
+    val blue: Color =
+        Color.rgb(19, 122, 227)
+
+    val light_blue: Color =
+        Color.rgb(75, 196, 245)
+
+    val purple: Color =
+        Color.rgb(170, 39, 251)
 }
